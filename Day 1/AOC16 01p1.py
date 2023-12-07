3c6426586e9eddb40320f61f6c006fbc94f32786
Hannah Walden <hannahfaithwalden@gmail.com>
2023-12-07 14:54:08 -0500

    Add Day 1

--- Day 1/AOC16 01p1.py
+++ Day 1/AOC16 01p1.py
@@ -1,10 +1,42 @@
-file_path = "Day X/test.txt"
+file_path = "Day 1/input.txt"
 
 with open(file_path, 'r') as file:
     lines_array = [line.strip() for line in file.readlines()]
 
-for lin in lines_array:
+moves = lines_array[0].split(", ")
 
+print(moves)
+cardinal = 0
+# 0 = north
+# 1 = east
+# 2 = south
+# 3 = west
+north = 0
+south = 0
+east = 0
+west = 0
 
+for each in moves:
+    if each[0] == "R":
+        cardinal += 1
+    elif each[0] == "L":
+        cardinal -= 1
 
-    print(lin)
+    if cardinal == 4:
+        cardinal = 0
+    if cardinal == -1:
+        cardinal = 3
+
+    if cardinal == 0:
+        north += int(each[1:])
+    elif cardinal == 1:
+        east += int(each[1:])
+    elif cardinal == 2:
+        south += int(each[1:])
+    elif cardinal == 3:
+        west += int(each[1:])
+    else:
+        print("Error")
+
+print(f"North: {north}    South: {south}    East: {east}    West: {west}")
+print(f"Total: {abs(north - south) + abs(east - west)}")
--- Day 1/AOC16 01p2.py
+++ Day 1/AOC16 01p2.py
@@ -1,10 +1,89 @@
-file_path = "Day X/test.txt"
+file_path = "Day 1/input.txt"
 
 with open(file_path, 'r') as file:
     lines_array = [line.strip() for line in file.readlines()]
 
-for lin in lines_array:
+moves = lines_array[0].split(", ")
+cardinal = 0
+# 0 = north
+# 1 = east
+# 2 = south
+# 3 = west
+
+coordinates = (0,0)
+locations = []
+flag = True
+
+for each in moves:
+    if flag:
+        if each[0] == "R":
+            cardinal += 1
+        elif each[0] == "L":
+            cardinal -= 1
+
+        if cardinal == 4:
+            cardinal = 0
+        if cardinal == -1:
+            cardinal = 3
+
+        r = int(each[1:])
+        if cardinal == 0:
+            for step in range(1,r+1):
+                temp_coordinates = (coordinates[0], coordinates[1] + step)
+
+                if (temp_coordinates[0], temp_coordinates[1]) in locations:
+                    print(f"First location visited twice: {(temp_coordinates[0], temp_coordinates[1])}")
+                    print(f"Distance: {abs(temp_coordinates[0]) + abs(temp_coordinates[1])}")
+                    flag = False
+                    break
+                else:
+                    locations.append((temp_coordinates[0], temp_coordinates[1]))
+
+            coordinates = temp_coordinates
+
+        elif cardinal == 1:
+            for step in range(1,r+1):
+                temp_coordinates = (coordinates[0] + step, coordinates[1])
+
+                if (temp_coordinates[0], temp_coordinates[1]) in locations:
+                    print(f"First location visited twice: {(temp_coordinates[0], temp_coordinates[1])}")
+                    print(f"Distance: {abs(temp_coordinates[0]) + abs(temp_coordinates[1])}")
+                    flag = False
+                    break
+                else:
+                    locations.append((temp_coordinates[0], temp_coordinates[1]))
+
+            coordinates = temp_coordinates
 
 
+        elif cardinal == 2:
+            for step in range(1,r+1):
+                temp_coordinates = (coordinates[0], coordinates[1] - step)
 
-    print(lin)
+                if (temp_coordinates[0], temp_coordinates[1]) in locations:
+                    print(f"First location visited twice: {(temp_coordinates[0], temp_coordinates[1])}")
+                    print(f"Distance: {abs(temp_coordinates[0]) + abs(temp_coordinates[1])}")
+                    flag = False
+                    break
+                else:
+                    locations.append((temp_coordinates[0], temp_coordinates[1]))
+
+            coordinates = temp_coordinates
+
+
+        elif cardinal == 3:
+            for step in range(1,r+1):
+                temp_coordinates = (coordinates[0] - step, coordinates[1])
+
+                if (temp_coordinates[0], temp_coordinates[1]) in locations:
+                    print(f"First location visited twice: {(temp_coordinates[0], temp_coordinates[1])}")
+                    print(f"Distance: {abs(temp_coordinates[0]) + abs(temp_coordinates[1])}")
+                    flag = False
+                    break
+                else:
+                    locations.append((temp_coordinates[0], temp_coordinates[1]))
+
+            coordinates = temp_coordinates
+
+        else:
+            print("Error")
